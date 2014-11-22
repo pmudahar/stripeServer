243b4e80222fd94915a0935dd300b9841be3aadd
pmudahar <paul.mudahar@gmail.com>
2014-11-22 17:02:23 -0500

    changed header

--- main.js
+++ main.js
@@ -23,7 +23,7 @@ app.set('view engine', 'jade');
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8100');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
